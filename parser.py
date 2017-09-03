111c2ba87f8532d2b6fcf32398a76a86ed42130c
NuclearRazor <nuclearrazor@gmail.com>
2017-09-03 22:51:04 +0300

    major update - add skinsjar parsing data
    - clear code

--- parser.py
+++ parser.py
@@ -28,15 +28,17 @@ class ParseMarkets(mc.MetaConfig):
 
         comission_list = self.get_comission()
 
-        check_csgotm = self.parse_csgotmmarket(comission_list[0])
-
         check_csmoney = self.parse_csmoneymarket(comission_list[1])
+
+        check_skinsjar = self.parse_skinsjarmarket(comission_list[2])
 
         check_csgosell = self.parse_csgosellmarket(comission_list[3])
 
-        print("\nStarted. TIME: "+start_fx)
+        check_csgotm = self.parse_csgotmmarket(comission_list[0])
 
-        print("Finished. TIME: "+repr(time.ctime()))
+        print("\nStarted. TIME: " + start_fx)
+
+        print("Finished. TIME: " + repr(time.ctime()))
 
 
     def convert_to_str(self, numlist):
@@ -173,8 +175,60 @@ class ParseMarkets(mc.MetaConfig):
         print('\n=====Csgosell parsing is done=====\n')
 
 
-    def csmoney_usd_course(self):
+    def parse_skinsjarmarket(self, site_comission):
+        print('\n=====Parse data from https://skinsjar.com=====')
 
+        skinsjar_url = 'https://skinsjar.com/api/v3/load/bots?refresh=0&v=0'
+
+        site_data = self.get_url_regular(skinsjar_url)
+
+        data = []
+        name = []
+        short_name = []
+        price = []
+        each_el = []
+        float_val = []
+        ext = []
+        priceToWrite = []
+        row_index = []
+        row_value = 0
+        data = json.loads(site_data)
+
+        for each in data['items']:
+            each_el.append(each)
+            if 'name' in each:
+                name.append(each['name'])
+                row_index.append(row_value)
+                row_value += 1
+            if 'shortName' in each:
+                short_name.append(each['shortName'])
+            if 'exterior' in each:
+                ext.append(each['exterior'])
+
+        for item in each_el:
+            if 'price' in item:
+                price.append(item['price'])
+            if 'floatMax' in item:
+                float_val.append(item['floatMax'])
+
+        #print('\nURL of skinsjar.com/ru/ database: ', skinsjar_url)
+
+        convert_course = self.csmoney_usd_course()
+
+        skinsjar_comission = int(site_comission)/100
+
+        #print("\nComission: ", csgosell_comission)
+
+        skinsjar_fixed_price = self.evaluate_price(price, skinsjar_comission, convert_course)
+
+        skinsjar_header = ["index", "c_market_name_en", "c_price", "c_quality"]
+        my_df = pd.DataFrame(list(map(list, zip(row_index, name, skinsjar_fixed_price, ext))), columns=skinsjar_header)
+        my_df.to_csv('skinsjar_data.csv', index=False)
+
+        print('\n=====Skinsjar parsing is done=====\n')
+
+
+    def csmoney_usd_course(self):
         money_url = 'https://cs.money/get_info?hash='
 
         money_pattern = r'(\d+\.\d+)'
@@ -220,72 +274,6 @@ class ParseMarkets(mc.MetaConfig):
         'qualitys': quality_items, 'prices': price_items, 'floats': float_items}
 
         return json_dict
-
-# def show_ip():
-#     try:
-#         url = 'http://whatismycountry.com'
-#         r = requests.Session()
-#         page = r.get(url)
-#         soup = BeautifulSoup(page.content, 'html5lib')
-#         h3 = soup.find('h3')
-#         strong = soup.find('strong')
-#         print('\nCountry: ', str(h3.get_text()).strip())
-#         print('\nIP: ', str(strong.get_text()).strip())
-#     except:
-#         print('\nCannot parse IP, continue')
-
-#site banned
-def skinsjar_market(convert_value, skinsjar_comission):
-    skinsjar_url = 'https://skinsjar.com/api/v3/load/bots?refresh=0&v=0'
-
-    r = requests.get(skinsjar_url)
-
-    webpage = r.content.decode('utf-8')
-
-    data = []
-    name = []
-    short_name = []
-    price = []
-    each_el = []
-    float_val = []
-    ext = []
-    priceToWrite = []
-    row_index = []
-    row_value = 0
-    data = json.loads(webpage)
-
-    for each in data['items']:
-        each_el.append(each)
-        if 'name' in each:
-            name.append(each['name'])
-            row_index.append(row_value)
-            row_value += 1
-        if 'shortName' in each:
-            short_name.append(each['shortName'])
-        if 'exterior' in each:
-            ext.append(each['exterior'])
-
-    for item in each_el:
-        if 'price' in item:
-            price.append(item['price'])
-        if 'floatMax' in item:
-            float_val.append(item['floatMax'])
-
-    print('\nURL of skinsjar.com/ru/ database: ', skinsjar_url)
-
-    COMISSION = int(skinsjar_comission) / 100
-    print("\nComission: ", COMISSION)
-
-    for price_element in price:
-        price_value = float(price_element) * (1 + COMISSION) * convert_value
-        price_value = round(price_value, 2)
-        priceToWrite.append(price_value)
-
-    skinsjar_header = ["index", "c_market_name_en", "c_price", "c_quality"]
-    my_df = pd.DataFrame(list(map(list, zip(row_index, short_name, priceToWrite, ext))), columns=skinsjar_header)
-    my_df.to_csv('skinsjar_data.csv', index=False)
-
-    print('\n=====Skinsjar parsing is done=====\n')
 
 
 if __name__ == '__main__':
