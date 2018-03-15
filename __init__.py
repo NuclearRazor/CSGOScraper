d3c136e563fae87014e7b9a46effb3e0eedd0806
NuclearRazor <nuclearrazor@gmail.com>
2018-03-16 00:06:50 +0300

    init folder fix

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 19.08.2017
-
-@author: NuclearRazor
-"""
-
---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 19.08.2017
+
+@author: NuclearRazor
+"""
+
